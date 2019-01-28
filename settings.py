5580e4824673a97c507ae72860de6f255a4cd010
anderson9212 <anderson_fos@hotmail.com>
2019-01-28 18:07:49 -0300

    Alterando o domínio no allowed hosts

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['bitcoin.debr.com.br', '127.0.0.1']
+ALLOWED_HOSTS = ['bitcoin.debr.com.br', '127.0.0.1', '3.16.243.115']
 
 
 INSTALLED_APPS = [
