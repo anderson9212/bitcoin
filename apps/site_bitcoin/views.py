34a27ee009a30e43ba529d41a3c5b39d9a604e5d
Anderson <anderson_fos@hotmail.com>
2019-01-29 00:36:09 -0300

    Melhorias no visual(css)

--- apps/site_bitcoin/views.py
+++ apps/site_bitcoin/views.py
@@ -4,9 +4,16 @@ from apps.site_bitcoin.forms import SendForm, ReceiveForm
 import requests
 
 # Dicionário python com principais parâmetros do sistema
-data_blockcypher = {
+"""data_blockcypher = {
     'address': 'C1meuDnZ4RNBjDhZD6FwLVhG1ecr4sDhfZ',
     'private': '1c8134cbcbeedebee9ff5add6224cf542ec8d8c82f45d650d404bfb0e4a1aacf',
+    'api_key': 'ca2a67fd0abf46cd938570f4f54b2b1f',
+    'url': 'https://api.blockcypher.com/v1/bcy/test/txs/micro',
+}"""
+
+data_blockcypher = {
+    'address': 'CAJPpxQ68hoGzQQcdHn2ZxuS7a6kijpGwy',
+    'private': 'd3133c6bdd2e4f9c787c96ef1cb707f1c89f57794646cf2f13554d9e22a226c1',
     'api_key': 'ca2a67fd0abf46cd938570f4f54b2b1f',
     'url': 'https://api.blockcypher.com/v1/bcy/test/txs/micro',
 }
@@ -30,34 +37,47 @@ def transaction(request, hash):
 
 # Função para envio de frações de moedas
 def send(request):
+    # Verifica se method é do tipo POST
     if request.method == 'POST':
+        # Armazena os dados enviado pelo formulário na variável form
         form = SendForm(request.POST)
+        # Verifica se o valor submetido no formulário é válido
         if form.is_valid():
+            # Transforma o valor retornado em inteiro
             value = int(form.cleaned_data['value'])
-
+            # Preparando parâmetros de envio da moeda
             data = {'from_private': data_blockcypher['private'],
                     'to_address': data_blockcypher['address'], 'value_satoshis': value}
+            # Passando a api_token para validar o envio
             params = {'token': data_blockcypher['api_key']}
+            # Requisição post com informações sobre a transaçaão
             requests.post(data_blockcypher['url'], data=data, params=params)
-
+            # Redireciona para a home(página inicial)
             return redirect('home')
     else:
+        # Passa para a variável form os dados da classe form
         form = SendForm()
-
+    # Retorna a página HTML para enviar frações de moedas
     return render(request, 'send.html', {'form': form})
 
 
 # Função para receber frações de moedas
 def receive(request):
+    # Verifica se method é do tipo POST
     if request.method == 'POST':
+        # Armazena os dados enviado pelo formulário na variável form
         form = ReceiveForm(request.POST)
+        # Verifica se o valor submetido no formulário é válido
         if form.is_valid():
+            # Transforma o valor retornado em inteiro
             value = int(form.cleaned_data['value'])
+            # Função para receber moedas passando: endereço, satoshis e chave da api
             send_faucet_coins(address_to_fund=data_blockcypher['address'], satoshis=value,
                               api_key=data_blockcypher['api_key'])
-
+            # Redireciona para a home(página inicial)
             return redirect('home')
     else:
+        # Passa para a variável form os dados da classe form
         form = ReceiveForm()
-
+    # Retorna a página HTML para receber frações de moedas
     return render(request, 'receive.html', {'form': form})
